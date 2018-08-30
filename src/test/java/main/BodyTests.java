e93312f1f30134d2ae1b8b186dacfbfb4a6116bf
PavelNejinskiy <808nps@gmail.com>
2018-08-30 12:05:01 +0300

    refactoring

--- src/test/java/main/BodyTests.java
+++ src/test/java/main/BodyTests.java
@@ -1,10 +1,7 @@
 package main;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import utils.Tools;
 import utils.Variables;
 
--- src/test/java/utils/Tools.java
+++ src/test/java/utils/Tools.java
@@ -1,7 +1,5 @@
 package utils;
 
-import main.BodyTests;
-import main.GooglePage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
